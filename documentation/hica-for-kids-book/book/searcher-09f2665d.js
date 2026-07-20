9fef2641fab726f2e21e94472ac4d41a61739993
cladam <claes.adamsson@gmail.com>
2026-07-20 11:30:05 +0200

    chore: bump version and publish a new minor release
    Intent Log:
    - Bumping to a new minor release after having implemented std/streams and std/xform and updated all relevant documentation
    - Ran mdbook locally so many files were updated but no new content to the hica for kids book

--- documentation/hica-for-kids-book/book/searcher-09f2665d.js
+++ documentation/hica-for-kids-book/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-068794fc.js',
+                path_to_root + 'searchindex-49082118.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
